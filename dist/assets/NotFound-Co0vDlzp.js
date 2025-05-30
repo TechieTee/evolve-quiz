8f5f9ed0381f9f7c8d9ccbd9bf45dbe4b5be30ad
Fatima Modupe Bello <mailtomodupe@gmail.com>
2025-05-30 18:16:13 +0100

    css updates

--- dist/assets/NotFound-Co0vDlzp.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-mLfMws2q.js";const r=()=>o.jsx("h1",{children:"404 - Page Not Found"});export{r as default};
---
+++ dist/assets/NotFound-wNX3qg1O.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DUc6P3Uf.js";const r=()=>o.jsx("h1",{children:"404 - Page Not Found"});export{r as default};
